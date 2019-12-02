587c034d6f27b9fe6872931f102fb20b5f8c4b98
Jon R. Roma <roma@illinois.edu>
2019-12-02 16:03:07 -0600

    Fix provider lines
    Quoted provider references are deprecated in Terraform.

--- acm.tf
+++ acm.tf
@@ -6,7 +6,8 @@ locals {
 data "aws_acm_certificate" "selected" {
   count = local.create_acm_cert ? 0 : 1
 
-  provider    = "aws.us-east-1"
+  provider = aws.us-east-1
+
   domain      = local.fqdn
   statuses    = ["ISSUED"]
   most_recent = true
@@ -15,7 +16,8 @@ data "aws_acm_certificate" "selected" {
 resource "aws_acm_certificate" "default" {
   count = local.create_acm_cert ? 1 : 0
 
-  provider                  = "aws.us-east-1"
+  provider = aws.us-east-1
+
   domain_name               = local.fqdn
   subject_alternative_names = var.aliases
   validation_method         = "DNS"
@@ -28,18 +30,20 @@ resource "aws_acm_certificate" "default" {
 resource "aws_route53_record" "acm" {
   count = local.create_acm_cert ? length(var.aliases) + 1 : 0
 
-  provider = "aws.us-east-1"
-  name     = aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_name"]
-  type     = aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_type"]
-  zone_id  = data.aws_route53_zone.selected[0].zone_id
-  ttl      = 60
-  records  = [aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_value"]]
+  provider = aws.us-east-1
+
+  name    = aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_name"]
+  type    = aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_type"]
+  zone_id = data.aws_route53_zone.selected[0].zone_id
+  ttl     = 60
+  records = [aws_acm_certificate.default[0].domain_validation_options[count.index]["resource_record_value"]]
 }
 
 resource "aws_acm_certificate_validation" "default" {
   count = local.create_acm_cert ? 1 : 0
 
-  provider                = "aws.us-east-1"
+  provider = aws.us-east-1
+
   certificate_arn         = aws_acm_certificate.default[0].arn
   validation_record_fqdns = aws_route53_record.acm.*.fqdn
 }
