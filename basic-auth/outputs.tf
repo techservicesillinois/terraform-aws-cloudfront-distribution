c138114cd7182868aa8c11051b6f08a3b5d1b225
Jon R. Roma <JonRoma@users.noreply.github.com>
2020-10-29 17:43:29 -0500

    Add dynamodb_table_name to outputs (#23)

--- basic-auth/outputs.tf
+++ basic-auth/outputs.tf
@@ -1,3 +1,7 @@
 output "policy_arn" {
   value = length(var.regions) > 0 && var.policy_name != "" ? aws_iam_policy.default[0].arn : null
 }
+
+output "dynamodb_table" {
+  value = length(var.regions) > 0 ? aws_dynamodb_global_table.default : null
+}
